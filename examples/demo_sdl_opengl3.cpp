baf6407530e15a7197b5104f45a6f16f92362b88
Francesco Abbate <francesco.bbt@gmail.com>
2021-05-04 15:10:04 +0200

    Use SDL_WaitEventTimeout instead of polling

--- examples/demo_sdl_opengl3.cpp
+++ examples/demo_sdl_opengl3.cpp
@@ -143,6 +143,8 @@ int main(int, char**)
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    const int demo_fps = 60;
+
     // Main loop
     bool done = false;
     while (!done)
@@ -153,7 +155,7 @@ int main(int, char**)
         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
-        while (SDL_PollEvent(&event))
+        while (SDL_WaitEventTimeout(&event, 1000 / demo_fps))
         {
             ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
